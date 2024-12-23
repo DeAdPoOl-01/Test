25ef6de2222f602e91708986e3732bb04d7c351b
DeAdPoOl-01 <mirazimmirzakhmedov@gmail.com>
2024-12-23 20:35:04 +0500

    Update test_bot.py
    version 2

--- test_bot.py
+++ test_bot.py
@@ -30,3 +30,34 @@ async def handle_text(message: types.Message):
         await check_menu(message)
     elif 'tovar' in user_data[user_id]['holat']:
         await check_items(message)
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    user_id = message.from_user.id
+    user_data[user_id] = {}
+    button = [
+        [types.KeyboardButton(text="Raqam jo'natish", request_contact=True)]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=button, resize_keyboard=True)
+    await message.answer("Assalomu alaykum! \n Les Ailes yetkazib berish xizmatiga xush kelibsiz:", reply_markup=keyboard)
+    print(user_data)
+
+
+async def send_code(message: types.Message):
+    user_id = message.from_user.id
+    i = '+1234567890'
+    ok = True
+    if message.contact is not None:
+        phone_c = message.contact.phone_number
+        user_data[user_id]['phone'] = phone_c
+        verification_code = randint(100000, 999999)
+        user_data[user_id]['verification_code'] = verification_code
+        await message.answer(f"Nomerizga tasdiqlash ko'di yuborildi\n"
+                         f"Iltimos kodni kiriting: {verification_code}")
+    elif len(message.text) == 13 and message.text[0:4]  == '+998':
+         for symbol in message.text:
+             if symbol not in i:
+                await message.answer('Hato nomer kiritildi')
+                var = ok == False
+                break
+
